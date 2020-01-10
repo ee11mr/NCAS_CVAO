9ad138660c20c8ca6db1b9bd679fdcc2d6558f14
ee11mr <ee11mr@leeds.ac.uk>
2020-01-10 11:23:05 +0000

    Tidying func fit code - currently working but a mess

--- code/fitting_function_from_url.py
+++ code/fitting_function_from_url.py
@@ -3,8 +3,7 @@
 """
 Created on Wed Nov 27 11:54:35 2019
 
-@author: mjr583
-"""
+@author: mjr583"""
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -37,37 +36,37 @@ d = {
                 'abbr' : 'CO',
                 'unit': 'ppbv',
                 'scale' : '1e9',
-                'start_year' : '2006',
-                'mean_name' : '?'
+                'start_year' : '2016',
+                'mean_name' : 'carbon_monoxide_amean'
                 }
      }
 
-S = ['O3']
-unit='ppbv'
-for n,species in enumerate(S):
-    url = 'http://thredds.nilu.no/thredds/dodsC/ebas/CV0001G.20061002000000.20190425081904.uv_abs.ozone.air.12y.1h.GB12L_CVO_Ozone_Thermo49series.GB12L_Thermo.lev2.nc'
+for s in d:
+    species = d[s]['longname']
+    url = d[s]['url']
     dataset = netCDF4.Dataset(url)
-    species='ozone'
-    spec=species
-    start_year='2006'
+    
+    try:
+        start_year = eval(dataset.comment)['Startdate'][:4]
+    except:
+        start_year=d[s]['start_year']
     
     time = dataset.variables['time'][:]
     new_date=np.array(timestamp_to_date(time))
-    mean = dataset.variables['ozone_nmol_per_mol_amean'][:] 
+    mean = dataset.variables[d[s]['mean_name']][:] 
     dtf = pd.DataFrame(mean)
     dtf.index = new_date
-    dtf.columns = [species]
+    dtf.columns = [s]
     df = dtf.resample('M').mean()
     
     XX = np.arange(len(df))
-    idx = np.isfinite(df.ozone)
-    
-    Y = df[idx].ozone 
+    idx = np.isfinite(df[s])    
+    Y = df[idx][s]
     X = XX[idx]
 
     ''' Guess of polynomial terms '''
     z, p = np.polyfit(X, Y, 1)
-    a = np.nanmean(df[spec][start_year].resample('A').mean())  #31.08
+    a = np.nanmean(df[s][start_year].resample('A').mean())  #31.08
     b = z
     c2 = .00001
     A1 = 5.1 
@@ -111,8 +110,8 @@ for n,species in enumerate(S):
     ax1.plot(X, var)
     plt.text(.75,.1, 'RMSE='+str(rmse), fontsize=14,transform=ax1.transAxes,)#, boxstyle='round,pad=1'))
     plt.xticks(np.arange(0, len(X), 12), years)
-    plt.close()
-    
+    #plt.close()
+    stop
     detrended = [Y[i] - c[1]*i for i in range(0, len(X))]
     detrended2 = np.array([detrended[i] - c[2]*i**2 for i in range(0, len(X))])
     
@@ -121,8 +120,9 @@ for n,species in enumerate(S):
     plt.plot(X,detrended2,label='Detrend b + c')
     plt.xticks(np.arange(0, len(X), 12), years)
     plt.legend()
+    plt.savefig(savepath+d[s]['longname']+'_trend_breakdown.png')
     plt.close()
-    
+
     ds = pd.DataFrame(detrended2[:])
     ds.index = df[idx].index
     seas = ds.groupby(ds.index.month).mean()
@@ -130,29 +130,29 @@ for n,species in enumerate(S):
     ####### Redo with monthly means replacing NaNs ###########
     df = dtf[start_year:].resample('M').mean() 
     for i,ii in enumerate(df.index.month):
-        if df[spec][i] != df[spec][i]:
+        if df[s][i] != df[s][i]:
             year=df.index[i].year
             try:
-                val = ( df[spec][str(year-1)][ii] + df[spec][str(year+1)][ii] ) / 2
-                df[spec][i] = val
+                val = ( df[s][str(year-1)][ii] + df[s][str(year+1)][ii] ) / 2
+                df[s][i] = val
             except:
                 try:
-                    val = df[spec][str(year-1)][ii] 
+                    val = df[s][str(year-1)][ii] 
                 except:
                     try:
-                        val = df[spec][str(year+1)][ii]  
+                        val = df[s][str(year+1)][ii]  
                     except:
                         pass
     
-    idx = np.isfinite(df[spec])
-    Y = df[spec][idx] 
+    idx = np.isfinite(df[s])
+    Y = df[s][idx] 
     X = np.arange(len(Y))
     #pp = np.linspace(1,5.1,np.round(len(Y)*0.75),0) ; qq = np.linspace(5.1,0.9,np.round(len(Y)*0.25,0)) ; rr = np.concatenate((pp,qq))
     #Y = Y * rr
     ''' Guess of polynomial terms '''
     z, p = np.polyfit(X, Y, 1)
     
-    a = np.nanmean(df[spec][start_year].resample('A').mean())  #31.08
+    a = np.nanmean(df[s][start_year].resample('A').mean())  #31.08
     b = z
     
     def new_func(x,m,c,c0):
@@ -193,12 +193,12 @@ for n,species in enumerate(S):
     ax1.plot(X, Y, 'ro', markersize=0.5)
     ax1.plot(X, y, '--')
     ax1.plot(X, var)
-    ax1.set_ylabel(species+unit)
-    txt = AnchoredText('RMSE='+str(rmse)+' '+unit[2:-2]+'\n$r^2$='+str(r2)+'%', loc=2)
+    ax1.set_ylabel(d[s]['unit'])
+    txt = AnchoredText('RMSE='+str(rmse)+' '+d[s]['unit']+'\n$r^2$='+str(r2)+'%', loc=2)
     ax1.add_artist(txt)
     #plt.text(.75,h, 'RMSE='+str(rmse)+' '+unit[2:-2]+'\n$r^2$='+str(r2)+'%', fontsize=12,transform=ax1.transAxes,)#, boxstyle='round,pad=1'))
     plt.xticks(np.arange(0, len(X), 12), years)
-    plt.savefig(savepath+species+'_nonlin_regression.png')
+    plt.savefig(savepath+d[s]['longname']+'_nonlin_regression.png')
     plt.close()
     
     detrended = [Y[i] - c[1]*i for i in range(0, len(X))]
@@ -215,4 +215,4 @@ for n,species in enumerate(S):
     ds = pd.DataFrame(detrended2[:])
     ds.index = df[idx].index
     seas = ds.groupby(ds.index.month).mean()
-    print(species, 'done')
+    print(s, 'done')
